ada4fc96de0799833f25688dc2a1ace944f9536b
charlesuu <tiger0192004@gmail.com>
2023-07-24 04:03:11 +0900

    feat: Car Controller 구현

---
+++ src/main/java/com/example/rentalcars/controller/car/CarController.java
@@ -0,0 +1,37 @@
+package com.example.rentalcars.controller.car;
+
+import com.example.rentalcars.dto.car.request.CarCreateRequest;
+import com.example.rentalcars.dto.car.request.CarRentalRequest;
+import com.example.rentalcars.dto.car.request.CarReturnRequest;
+import com.example.rentalcars.service.car.CarService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class CarController {
+
+    private final CarService carService;
+
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
+    @PostMapping("/car")
+    public void saveCar(@RequestBody CarCreateRequest request) {
+        carService.saveCar(request);
+    }
+
+    @PostMapping("/car/rental")
+    public void rentCar(@RequestBody CarRentalRequest request) {
+        carService.rentCar(request);
+    }
+
+    @PutMapping("/car/return")
+    public void returnCar(@RequestBody CarReturnRequest request) {
+        carService.returnCar(request);
+    }
+
+}
