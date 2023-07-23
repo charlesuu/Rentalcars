75ac561ce2003233e8f496b7c2d5b69c91a36b58
charlesuu <tiger0192004@gmail.com>
2023-07-24 04:08:36 +0900

    feat: User JPA 버전 Service 구현

---
+++ src/main/java/com/example/rentalcars/service/user/UserServiceV2.java
@@ -0,0 +1,74 @@
+package com.example.rentalcars.service.user;
+
+import com.example.rentalcars.domain.user.User;
+import com.example.rentalcars.domain.user.UserRepository;
+import com.example.rentalcars.dto.user.request.UserCreateRequest;
+import com.example.rentalcars.dto.user.request.UserUpdateRequest;
+import com.example.rentalcars.dto.user.response.UserResponse;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Primary
+public class UserServiceV2 implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceV2(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Transactional
+    public Long saveUser(UserCreateRequest request) {
+        return userRepository.save(new User(request.getName(), request.getAge())).getId();
+    }
+
+    @Transactional(readOnly = true)
+    public List<UserResponse> getUsers() {
+        List<User> users = userRepository.findAll();
+        return users.stream()
+                .map(user -> new UserResponse(user.getId(), user.getName(), user.getAge()))
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void updateUser(UserUpdateRequest request) {
+        User user = userRepository.findById(request.getId())
+                .orElseThrow(IllegalArgumentException::new);
+        user.updateName(request.getName());
+
+        //save생략 (영속성 컨택스트)
+    }
+
+    @Transactional
+    public void deleteUser(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(IllegalArgumentException::new);
+
+        userRepository.delete(user);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
