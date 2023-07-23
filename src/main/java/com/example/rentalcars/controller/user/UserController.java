0abe6b406ff722c105812d1aeb0151d1138c28e2
charlesuu <tiger0192004@gmail.com>
2023-07-24 04:03:21 +0900

    feat: User Controller 구현

---
+++ src/main/java/com/example/rentalcars/controller/user/UserController.java
@@ -0,0 +1,62 @@
+package com.example.rentalcars.controller.user;
+
+
+import com.example.rentalcars.dto.user.request.UserCreateRequest;
+import com.example.rentalcars.dto.user.request.UserUpdateRequest;
+import com.example.rentalcars.dto.user.response.UserResponse;
+import com.example.rentalcars.service.user.UserService;
+import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {//userServiceV2가 primary
+        this.userService = userService;
+    }
+
+    @PostMapping("/user")
+    public Long saveUser(@RequestBody UserCreateRequest request) {
+        return userService.saveUser(request);
+    }
+
+    @GetMapping("/user")
+    public List<UserResponse> getUsers() {
+        return userService.getUsers();
+    }
+
+    @PutMapping("/user")
+    public void updateUser(@RequestBody UserUpdateRequest request) {
+        userService.updateUser(request);
+    }
+
+    @DeleteMapping("/user/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
