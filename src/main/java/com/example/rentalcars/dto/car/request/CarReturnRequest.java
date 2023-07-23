2b52640b1d978aaa9790bf286f7afd43e7afca40
charlesuu <tiger0192004@gmail.com>
2023-07-24 04:05:09 +0900

    feat: Car ReturnRequestDto 구현

---
+++ src/main/java/com/example/rentalcars/dto/car/request/CarReturnRequest.java
@@ -0,0 +1,15 @@
+package com.example.rentalcars.dto.car.request;
+
+public class CarReturnRequest {
+
+    String userName;
+    String carName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+}
