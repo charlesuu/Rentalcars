b03d00b265ae8ac9f9a21d7bd966226a783f13e6
charlesuu <tiger0192004@gmail.com>
2023-07-24 04:06:09 +0900

    feat: User RequestDto 구현

---
+++ src/main/java/com/example/rentalcars/dto/user/request/UserUpdateRequest.java
@@ -0,0 +1,15 @@
+package com.example.rentalcars.dto.user.request;
+
+public class UserUpdateRequest {
+
+    private String name;
+    private long id;
+
+    public String getName() {
+        return name;
+    }
+
+    public long getId() {
+        return id;
+    }
+}
