4ab8f225cdb2cc83cbabf8e3f13ba6db6a7a2c8b
charlesuu <tiger0192004@gmail.com>
2023-07-24 04:06:25 +0900

    feat: User ResponseDto 구현

---
+++ src/main/java/com/example/rentalcars/dto/user/response/UserResponse.java
@@ -0,0 +1,35 @@
+package com.example.rentalcars.dto.user.response;
+
+
+import com.example.rentalcars.domain.user.User;
+
+public class UserResponse {
+
+    private long id;
+    private String name;
+    private Integer age;
+
+    public UserResponse(long id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public UserResponse(long id, User user) {
+        this.id = id;
+        this.name = user.getName();
+        this.age = user.getAge();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }//json으로 반환하기 위함임.
+}
